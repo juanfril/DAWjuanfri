1d82388106babff394fad77eb1d1349e48715dc0
juanfril <74176684+juanfril@users.noreply.github.com>
2020-12-09 21:45:39 +0100

    backup

--- Entornos de desarrollo/Bloq2/Unit2/reto1/src/Main.java
+++ Entornos de desarrollo/Bloq2/Unit2/reto1/src/Main.java
@@ -4,38 +4,33 @@ public class Main {
   public static void main(String[] args) {
     
     Scanner entrada = null;
-    try{
-        entrada = new Scanner(System.in);
-        System.out.println("Introduce el número de pruebas");
-        int pruebas = entrada.nextInt();
-        final int limiteCaracteres = 100;
-        String nombre ="";
-        String parentesco = "";
+    
+    entrada = new Scanner(System.in);
+    System.out.println("Introduce el número de pruebas");
+    int pruebas = entrada.nextInt();
+    final int limiteCaracteres = 100;
+    String nombre ="";
+    String parentesco = "";
 
-        for(int i = 0; i < pruebas; i++){
-            do{
-                System.out.println("Introduce el nombre y parentesco: ");
-                nombre = entrada.next();
-                parentesco = entrada.next().replace(" ", "");
+    for(int i = 0; i < pruebas; i++){
+        do{
+            System.out.println("Introduce el nombre y parentesco: ");
+            nombre = entrada.next();
+            parentesco = entrada.next().replace(" ", "");
 
-                if(nombre.length() > limiteCaracteres || parentesco.length() > limiteCaracteres) {
-                System.out.println("Número de carácteres máximo 100");
-                }
-
-            }while (nombre.length() > limiteCaracteres ||
-            nombre.contains(" ") || parentesco.contains(" ") || parentesco.length() > limiteCaracteres);
-           
-            if(nombre.equals("Luke") && parentesco.equals("padre")){
-                System.out.println("TOP SECRET");
+            if(nombre.length() > limiteCaracteres || parentesco.length() > limiteCaracteres) {
+            System.out.println("Número de carácteres máximo 100");
             }
-            else {
-                System.out.println(nombre + ", yo soy tu " + parentesco);
-            }        
-        }    
-    }
-    finally{
-        if(entrada!=null)
-            entrada.close();
-    }           
+
+        }while (nombre.length() > limiteCaracteres ||
+        nombre.contains(" ") || parentesco.contains(" ") || parentesco.length() > limiteCaracteres);
+        
+        if(nombre.equals("Luke") && parentesco.equals("padre")){
+            System.out.println("TOP SECRET");
+        }
+        else {
+            System.out.println(nombre + ", yo soy tu " + parentesco);
+        }        
+    }             
   }
 }
