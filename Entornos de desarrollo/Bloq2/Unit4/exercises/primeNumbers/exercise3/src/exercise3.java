3e7363b7c5c7fed6f66bb4056213cfb77700d8ab
juanfril <74176684+juanfril@users.noreply.github.com>
2020-12-19 13:48:03 +0100

    optionalExercises

---
+++ Entornos de desarrollo/Bloq2/Unit4/exercises/primeNumbers/exercise3/src/exercise3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercise3 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int[] primeNumbers = new int[10];
+        int input;
+
+
+        System.out.print("Input a number: ");
+        input = sc.nextInt();
+
+        for(int i = 1; i >= input; i++){
+
+        }
+
+        sc.close();
+    }
+}
