f7cedbbee56455373fb33886c1f4eaf4d37bd368
juanfril <juanfril@gmail.com>
2020-12-20 09:06:41 +0100

    keep java learning

--- Entornos de desarrollo/Bloq2/Unit4/exercises/exercise3/src/exercise3.java
+++ Entornos de desarrollo/Bloq2/Unit4/exercises/exercise3/src/exercise3.java
@@ -1,3 +1,5 @@
+//not finish
+
 import java.util.Scanner;
 
 public class exercise3 {
@@ -6,17 +8,17 @@ public class exercise3 {
 
         System.out.print("Input a number: ");
         int input = sc.nextInt();
-        boolean find = false;
 
         System.out.print("Prime numbers until " + input + ": ");
 
         for(int i = 1; i < input; i++){
 
-            for( int j = 2; j < input || find ; j++){
+            for( int j = 2; j <= input; j++){
                 if(i==1){
-                    System.out.print(1 + ",");
-                    find = true;
+                    System.out.print("1,");
                 }
+                else if(i%j != 0)
+                    System.out.print(i + ", ");
             }
         }
         
---
+++ Entornos de desarrollo/Bloq2/Unit4/exercises/exercise4/exercise4/src/exercise4.java
@@ -0,0 +1,41 @@
+//Juan Fco. Losa Márquez
+/*program that asks the user double numbers until the word “end” is
+typed and then it has to print the minimum, the maximum, the addition and
+the average in each step.*/
+
+import java.util.Scanner;
+
+public class exercise4 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        double num, minimun = 200000, maximun = -200000, addition = 0,
+            average;
+        int count = 0;
+        String exit;
+
+        do{
+            System.out.print("Number? ");
+            exit = sc.nextLine();
+            if(exit.equals("end"))
+                System.out.println("Thanks for use our program!");
+            
+            else{
+                num = Double.parseDouble(exit);
+                count++;
+                if(num < minimun)
+                    minimun = num;
+                if(num > maximun)
+                    maximun = num;
+                addition += num;
+                average = addition / count;
+
+                System.out.println("min= " + minimun + " max= " + maximun + 
+                " add= " + addition + " avg= " + average);
+            }
+            
+        }
+        while(!exit.equals("end"));
+
+        sc.close();
+    }
+}
