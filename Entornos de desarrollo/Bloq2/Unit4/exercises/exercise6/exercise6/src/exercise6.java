5007a85b2be59c8c5933cde44a024998e603854c
juanfril <juanfril@gmail.com>
2020-12-20 23:11:41 +0100

    keep learning java

---
+++ Entornos de desarrollo/Bloq2/Unit4/exercises/exercise6/exercise6/src/exercise6.java
@@ -0,0 +1,56 @@
+/*Juan Fco. Losa Márquez
+Ask the names of the students at the
+same time than their marks. You have to store the names in an array and the
+marks in another array and the link between them are the indexes, that is, the
+student in the first position corresponds to the first mark, the second to the
+second and so on. After storing these names and marks, the program must
+show the name of the students with marks greater than the average and the
+name of the student with the greatest mark.*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exercise6 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int maxArray;
+        double suma = 0, average;
+        double[] numbers;
+        String[] names;
+
+        System.out.print("How many marks are going to introduce? ");
+        maxArray = sc.nextInt();
+        numbers = new double[maxArray];
+        names = new String[maxArray];
+
+        for(int i = 0; i < maxArray; i++){
+            names[i] = sc.next();
+            numbers[i] = sc.nextDouble();
+            suma += numbers[i];
+        }
+        sc.close();
+
+        average = suma / (maxArray);
+
+        System.out.println("The average is: " + average);
+        System.out.println("The students with marks greater than" +
+         " the average are: ");
+
+        for (int i = 0; i < maxArray; i++){
+            if(average < numbers[i])
+                System.out.println("- " + names[i]);
+        }
+
+        for (int i = 0; i < maxArray; i++){
+            for ( int j = 1; j < maxArray; j++){
+                if (numbers[i] < numbers[j]){
+                    
+                }
+            }
+        }
+
+        System.out.print("The student with the greatest mark is: " + 
+        names[0]);
+
+    }
+}
