f4aaf651b0c48cc1a9a17a2bcbce822c522724fa
juanfril <74176684+juanfril@users.noreply.github.com>
2021-02-01 06:39:34 +0100

    no se

---
+++ Programación/Udemy/Ejemplos_Tema6_parteI/EjemploBiblioteca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Usuario usu1 = new Usuario("11122233A", "Nacho Iborra", "C/ CSharp, 11",
+            "966112233", false);
+
+        Console.WriteLine(usu1.GetNombre());
+    }
+}
