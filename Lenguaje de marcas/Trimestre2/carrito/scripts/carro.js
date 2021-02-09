c830ea016cdc79fdd70b9080ce235c3361181a32
juanfril <74176684+juanfril@users.noreply.github.com>
2021-02-09 13:42:16 +0100

    no veas

--- Lenguaje de marcas/Trimestre2/carrito/scripts/carro.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function(){
-    let color = Array.from.document.getElementsByClassName('.item');
-    for (const key of color) {
-        key.style.backgroundColor = '#cecece';   
-    }
-    
-})();
