c324c9bb9473c0cc09d22f320280c66ca614ff67
juanfril <74176684+juanfril@users.noreply.github.com>
2021-03-07 19:52:15 +0100

    no se

---
+++ Programación/2eval/Soluciones ejercicos clases/PersonajeMovil/PersonajeMovil/Partida.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonajeMovil
+{
+    /* Pantalla principal del juego, donde podemos mover al personaje */
+    class Partida
+    {
+        // Lanza la partida principal, dibujando las naves y enemigos e interactuando con el usuario (recogiendo los movimientos de la nave y demás)
+        public void Lanzar()
+        {
+            Personaje p = new Personaje();
+            p.MoverA(40, 12);
+            p.Dibujar();
+            ConsoleKeyInfo tecla;
+            do
+            {
+                tecla = Console.ReadKey();
+                Console.Clear();
+                if (tecla.Key == ConsoleKey.LeftArrow)
+                {
+                    p.MoverIzquierda();
+                }
+                else if (tecla.Key == ConsoleKey.RightArrow)
+                {
+                    p.MoverDerecha();
+                }
+                else if (tecla.Key == ConsoleKey.UpArrow)
+                {
+                    p.MoverArriba();
+                }
+                else if (tecla.Key == ConsoleKey.DownArrow)
+                {
+                    p.MoverAbajo();
+                }
+
+                p.Dibujar();
+            } while (tecla.Key != ConsoleKey.Escape);
+        }
+    }
+}
