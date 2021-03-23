ae76bd73598b5f980b697b8ea6fb7a7914eb6a6d
Juan Fco. Losa Marquez <74176684+juanfril@users.noreply.github.com>
2021-03-23 17:08:24 +0100

    git

---
+++ Development environm/Bloq2/AprendeJavaFx/src/module-info.java
@@ -0,0 +1,11 @@
+module AprendeJavaFx {
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.controls;
+    requires javafx.web;
+    requires javafx.base;
+    requires javafx.fxml;
+
+
+    opens programa;
+}
