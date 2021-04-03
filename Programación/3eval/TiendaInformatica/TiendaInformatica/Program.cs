62ca83bfc78310eab9b8e9e9a01ce19515679d92
juanfril <74176684+juanfril@users.noreply.github.com>
2021-04-03 13:28:50 +0200

    asf

---
+++ Programación/3eval/TiendaInformatica/TiendaInformatica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TiendaInformatica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programación/3eval/TiendaInformatica/TiendaInformatica/Utilidades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaInformatica
+{
+    class Utilidades
+    {
+        public static bool ComprobarCadena(string texto)
+        {
+            bool error = false;
+
+            if (String.IsNullOrEmpty(texto))
+                error = true;
+
+            return error;
+        }
+        public static bool ComprobarNumero(int numero)
+        {
+            bool error = false;
+
+            if (numero <= 0)
+                error = true;
+
+            return error;
+        }
+        public static bool ComprobarCodigo(string texto, SortedList catalogo)
+        {
+            bool error = false;
+
+            if (catalogo.ContainsKey(texto))
+                error = true;
+
+            return error;
+        }
+    }
+}
